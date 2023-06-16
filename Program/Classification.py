40b25ebb2b705623bf8ff4619fdb42a7ccbddf13
anhtu-pham <122914252+anhtu-pham@users.noreply.github.com>
2023-06-17 00:52:11 +0700

    Update code and trials

--- Program/Classification.py
+++ Program/Classification.py
@@ -18,8 +18,9 @@ from PIL import Image
 from tensorflow import keras
 
 link_address = "https://storage.googleapis.com/kaggle-data-sets/2779739/4804396/bundle/archive.zip?X-Goog-Algorithm=GOOG4-RSA-SHA256&X-Goog-Credential=gcp-kaggle-com%40kaggle-161607.iam.gserviceaccount.com%2F20230613%2Fauto%2Fstorage%2Fgoog4_request&X-Goog-Date=20230613T161356Z&X-Goog-Expires=259200&X-Goog-SignedHeaders=host&X-Goog-Signature=8f2db3da18017760aca09cbf043e89494a39c28c016784df584061898b4f4854b7fd9ae8f8ec7a36e113a1efd3cd30186c6fa96a253294829efbcd00c184e8b96b3d0acb9cf5e260944d2337689c62bc771a18ea28fd3bed4a3fcafcf38f70a8d179f280e8eb95e8c10aac90ee0b286af6cb8388560d2fc923a680485452994200ee4439f354bc00f363a55e69cfb3ff3a3634358435336182b4e4d6154e196b002ed4d1563fd9b17a1c06fcbcb3b0878a3a2e613365d4577b4bc0c3df9cf9e16d3df0e95a46c800850413cbf59c9d81120270fff9d613d0b594abada030ad54e5315a83e32ee1fa452cb7cbd3d827a9d8297c70e89e4656b2b1f5442f0c84d1"
-data_dir = keras.utils.get_file(origin = link_address, untar = True)
+data_dir = keras.utils.get_file(origin = link_address, extract = True, cache_dir = "./")
 data_dir = pathlib.Path(data_dir)
+print(data_dir)
 
 # data_dir = "../../Data Files/archive1"
 # data_dir = pathlib.Path(data_dir)
@@ -42,13 +43,25 @@ def is_jpeg(file_path):
         return False
 
 for folder_name in ("Academic_Art", "Art_Nouveau", "Baroque", "Expressionism", "Japanese_Art", "Neoclassicism", "Primitivism", "Realism", "Renaissance", "Rococo", "Romanticism", "Symbolism", "Western_Medieval"):
-    folder_path = os.path.join(data_dir, folder_name, folder_name)
+    folder_path = os.path.join(data_dir, "../", folder_name, folder_name)
+    print(folder_path)
     for fname in os.listdir(folder_path):
         fpath = os.path.join(folder_path, fname)
         if not is_jpeg(fpath):
             print(fpath)
-            os.remove(fpath)
+            # os.remove(fpath)
 
+sys.exit()
+
+# for folder_name in ("Academic_Art", "Art_Nouveau", "Baroque", "Expressionism", "Japanese_Art", "Neoclassicism", "Primitivism", "Realism", "Renaissance", "Rococo", "Romanticism", "Symbolism", "Western_Medieval"):
+#     folder_path = os.path.join(data_dir, folder_name, folder_name)
+#     for fname in os.listdir(folder_path):
+#         fpath = os.path.join(folder_path, fname)
+#         if not is_jpeg(fpath):
+#             print(fpath)
+#             os.remove(fpath)
+
+# Obsolete:
         # try:
         #     fobj = open(fpath, "rb")
         #     is_jfif = tf.compat.as_bytes("JPEG") in fobj.peek(10)
@@ -77,40 +90,29 @@ print("Classes' names:")
 print(class_names)
 number_of_styles = len(class_names)
 
-# image_shape = None
-# shape_found = False
-# for image_batch, labels_batch in train_dataset:
-#     if(shape_found is False):
-#         image_shape = image_batch.shape[1:]
-#         shape_found = True
-# tensorflow.image.rgb_to_grayscale(image_batch)
-    # image = image_batch.numpy()
-    # image_batch = image.astype("float32") / (np.max(image))
-    # print("np min:")
-    # print(np.min(image_batch))
-    # print("np max:")
-    # print(np.max(image_batch))
+image_shape = None
+for image_batch, labels_batch in train_dataset:
+    image_shape = image_batch.shape
+    break
 
-def preprocess_image(image, style):
-    # Check if the image is in grayscale
-    if image.shape[-1] == 1:
-        # Image is already grayscale, skip conversion
-        return image, style   
-    # Convert to grayscale if in RGB format
-    image = tf.image.rgb_to_grayscale(image)
-    # Resize the image to a fixed size
-    image = tf.image.resize(image, [224, 224])
-    # Normalize pixel values to the range [0, 1]
-    image = tf.cast(image, tf.float32) / 255.0
-    return image, style
+# def preprocess_image(image, style):
+#     # Check if the image is in grayscale
+#     if image.shape[-1] == 1:
+#         # Image is already grayscale, skip conversion
+#         return image, style   
+#     # Convert to grayscale if in RGB format
+#     image = tf.image.rgb_to_grayscale(image)
+#     # Resize the image to a fixed size
+#     image = tf.image.resize(image, [224, 224])
+#     # Normalize pixel values to the range [0, 1]
+#     image = tf.cast(image, tf.float32) / 255.0
+#     return image, style
 
-train_dataset = train_dataset.map(preprocess_image)
-validation_dataset = validation_dataset.map(preprocess_image)
-print("train dataset type")
-print(type(train_dataset))
+# train_dataset = train_dataset.map(preprocess_image)
+# validation_dataset = validation_dataset.map(preprocess_image)
 
 # write convolutional neural network model to classify styles of art
-def build_model(input_shape = (224, 224, 1), num_classes = number_of_styles):
+def build_model(input_shape = image_shape, num_classes = number_of_styles):
     # units = hp.Int('units', min_value = 64, max_value = 512, step = 32)
     
     model = keras.Sequential()
